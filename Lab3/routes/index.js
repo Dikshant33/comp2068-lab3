83f1c313a7b0fb8c5f3ca13ad49c7d3361ad2799
Dikshant Dudhat <dixantdudhat33@gmail.com>
2019-10-02 13:12:27 -0400

    first commit

---
+++ Lab3/routes/index.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+   
+});
+
+router.get('/dikshant', function (req, res) {
+    res.render('Dikshant', { title: 'Dikshant' });
+
+});
+
+router.get('/paul', function (req, res) {
+    res.render('Paul', { title: 'Paul' });
+
+});
+
+router.get('/tina', function (req, res) {
+    res.render('Tina', { title: 'Tina' });
+
+});
+
+router.get('/vraj', function (req, res) {
+    res.render('Vraj', { title: 'Vraj' });
+
+});
+
+module.exports = router;
